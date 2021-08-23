5a544a867abc3b89339a9b6d42795f28eaba40d6
Ankitp110 <ankitpanjwani05@gmail.com>
2021-08-23 08:20:56 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,4 @@
-resource "aws_instance" "test3" {
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = var.instancetype
-  //vpc_security_group_ids = ["sg-0e594a3fb2fc31ff6"]
-  count = 3
-  tags = {
-    Name = var.ec2_names[count.index]
-  }
+resource "aws_instance" "test-ec2" {
+  ami           = "ami-0c2b8ca1dad447f8a"
+  instance_type = "t2.micro"
 }
